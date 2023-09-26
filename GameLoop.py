15c06f9154c5fc26ee3481ea300d4f982bc338a6
reko <rekos766@gmail.com>
2023-09-26 23:04:10 +0300

    Game Loop

--- GameLoop.py
+++ GameLoop.py
@@ -1,7 +1,3 @@
-import time
-import math
-
-
 def airportdistance():
     airportdistance
     # lasketaan lentomatka lentokenttien välillä
@@ -18,6 +14,7 @@ starting_airport = "KJFK"  # tai airport[0]['ident']
 current_airport = starting_airport
 end_airport = "LAX"  # Los Angeles
 
+#test question
 questions = [
     {
         "question": "Mikä on kullan kemiallinen merkki?",
@@ -37,14 +34,13 @@ while not game_over:
 
     # Display player's current status
     airport = 'JFK Airport'  # Starting at JFK Airport
-    #print(f'{player_name}, you are at', airport)
     print(f"You have {money}$, {player_lives} lives left and {player_range} range")
     print("\033[34m---------------------------------------\033[0m")
 
-
+    # Ohjeita pelaajalle
     print("You need to answer questions to get range")
     print("You have 15 seconds to answer the question")
-    input("\033[34mPress Enter when you are ready to answer\033[0m")
+    input("\033[34mPress Enter when you are ready to answer\033[0m") # Pauses the game before question
     print(questions[0]['question'])
 
     answer = input("Enter your answer: ")
@@ -63,11 +59,12 @@ while not game_over:
 
 
     airportdistance = 500 # need to calculate distance between airports
+
     if player_range >= airportdistance:
         print("You have enough range to get to your next destination")
         input("\033[32mPress Enter to use your range\033[0m")
 
-        #current_airport = next_airport  # siirtyy seuraavaan lentokenttään
+        # siirtyy seuraavaan lentokenttään
         print("Welcome to {next_airport}")
         player_range -= airportdistance
 
